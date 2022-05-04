03b65cd32a192008d54f2d685406df8cad1fac43
nusrat78 <njahan5061@gmail.com>
2022-05-05 01:51:13 +0600

    tittle set

---
+++ src/components/ShopByIndustry/ShopByIndustry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopByIndustry = () => {
+    return (
+        <div>
+            <h2>Industry</h2>
+        </div>
+    );
+};
+
+export default ShopByIndustry;
